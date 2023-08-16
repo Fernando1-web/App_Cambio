5278c02e20951335192227ab90c63ed356912195
28904 <28904@KNOBBLEBOY>
2023-08-16 05:51:08 -0600

    Agregar archivos de proyecto.

---
+++ App_Cambio/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace App_Cambio;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void BtnCalcular_Clicked(object sender, EventArgs e)
+    {
+		double Total, Pago, Cambio;
+
+		Total = Convert.ToDouble(EntryTotal.Text);
+		Pago = Convert.ToDouble(EntryPago.Text);
+
+		Cambio = Pago - Total;
+
+		LblResultado.Text = "$" + Cambio.ToString();
+    }
+}
+
